a82fa383349990cc04ea09251d28857d9625dbfe
Iagoces <66168503+Iagoces@users.noreply.github.com>
2022-06-20 14:46:19 -0300

    Update CANARIO_SLAVE_NANO.ino

--- CANARIO_SLAVE_NANO.ino
+++ CANARIO_SLAVE_NANO.ino
@@ -1,14 +1,19 @@
 #include <Wire.h>
 
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+//Pino em que o sensor esta conectado
+const int analogInPin = A0;
+
+//Variaveis Globais
 int sensorValue = 0;
 
 void setup(){
+  //Inicial o Slave no Endreço 0x0A
   Wire.begin(10);
-  Serial.begin(9600);
+  //Cria a funcao para quando o mestre pede uma leitura
   Wire.onRequest(Read_Lum);
 }
 
+//Funcao para enviar o valor lido
 void Read_Lum(){
    delay(2000);
    Wire.write(sensorValue);
@@ -16,5 +21,6 @@ void Read_Lum(){
 
 void loop(){
    delay(2000);
+  //Le o valor do sensor e divide por 4 para poder enviar um de 8 bits que é o suportado pelo I2C
    sensorValue = analogRead(analogInPin)/4;
 }
