b81ac09c8af38f57ec4f938375a36e5a00bef984
Iagoces <66168503+Iagoces@users.noreply.github.com>
2022-06-20 14:48:40 -0300

    Update CANARIO_SLAVE_UNO.ino

--- CANARIO_SLAVE_UNO.ino
+++ CANARIO_SLAVE_UNO.ino
@@ -1,26 +1,32 @@
 #include <Wire.h>
 #include <DHT.h>
 
-
+//Define o pino em que o sensor esta conectado e o tipo do sensor
 #define DHTPIN 8
 #define DHTTYPE    DHT11
+
+//Cria as variaveis globais
 int  temp=0;
 
+//Inicial o dht no pino e no tipo do sensor
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup(){
+  //Inicia o I2C no endereco 0x0B
   Wire.begin(11);
   dht.begin();
   Wire.onRequest(Read_Temp);
 }
 
+//Funcao para enviar a temperatura lida
 void Read_Temp(){
    delay(2000);
-   Wire.write(temp);
-
-   
+   Wire.write(temp); 
 }
+
+
 void loop() {
    delay(2000);
+  //Faz leitura do sensor
    temp= dht.readTemperature();
 }
